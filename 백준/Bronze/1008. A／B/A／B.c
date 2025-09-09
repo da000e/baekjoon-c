9b99ef1d8b0bf02e55f9f728d25115f5b496a378
Junu.lee <dadada000e@gmail.com>
2025-09-10 02:29:18 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.c
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    float a, b;
-    scanf("%f %f",&a,&b);
-    printf("%.9f",(double)a/b);
+    double a, b;
+    scanf("%lf %lf",&a,&b);
+    printf("%.9lf",a/b);
     return 0;
 }
